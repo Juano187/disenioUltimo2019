5ecbe70d86618d9fbb39793eef03a3da9de87f4f
juano187 <jmauro@frsf.utn.edu.ar>
2019-01-18 17:53:30 -0300

    primero

--- src/main/java/Controlador/Principal.java
+++ src/main/java/Controlador/Principal.java
@@ -15,14 +15,13 @@ public class Principal {
     Inicializador01 cp = new Inicializador01();
     
         cp.setVisible(true);
-        EjemploError b = new EjemploError(" HOLA TOTI");
+     
      IniciarSesionCU00 a = new IniciarSesionCU00();
-     System.out.println("HOLA TOTI COMO VA LA BAINA");
+   
       //ActualizarEstadoIntervencionCU08 a = new ActualizarEstadoIntervencionCU08();
        	cp.dispose();
        	
       a.setVisible(true);
-      b.setVisible(true);  
 
     }
     
